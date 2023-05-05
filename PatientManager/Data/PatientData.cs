68c7cdc871e0af917977efb01561d4b4ad695f28
AndresMena <andresantiagomenangulo@gmai.com>
2023-05-04 23:55:51 -0400

    Agrgacion de bibliotecas de librerias patientmanager y applicationexception

---
+++ PatientManager/Data/PatientData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientManager.Data
+{
+    internal class PatientData
+    {
+        public int CI { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string BloodType { get; set; }
+    }
+}
